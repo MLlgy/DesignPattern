0aba1db37e1222170b879493bcc3e51be7d62750
MLlgy <18363851609@163.com>
2018-09-03 10:51:48 +0800

    mediator

--- src/mediatorPattern/model/Client.java
+++ src/mediatorPattern/model/Client.java
@@ -3,6 +3,9 @@ package mediatorPattern.model;
 public class Client {
     public static void main(String[] args) {
         Mediator mediator = new ConcreteMediator();
-
+        BussinessObjectOne bussinessObjectOne = new BussinessObjectOne(mediator);
+        BussinessObjectTwo bussinessObjectTwo = new BussinessObjectTwo(mediator);
+        bussinessObjectOne.depMethod1();
+        bussinessObjectTwo.depMethod2();
     }
 }
